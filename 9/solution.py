bf053d2e3721276538b2895941d11d6b1d9baa97
florin-ulrich <florin.n.ulrich@gmail.com>
2023-12-10 23:46:42 +0100

    recursion solution day 9

--- 9/solution.py
+++ 9/solution.py
@@ -4,32 +4,24 @@ with open("9/input.txt", "r") as f:
     puzzle_input = f.readlines()
 
 
-def subsequence(sequence):
-    return [sequence[i] - sequence[i - 1] for i in range(1, len(sequence))]
-
-
-def next_value(sequence, index, sign):
-    stack = [sequence]
-    while not all(x == 0 for x in stack[-1]):
-        stack.append(subsequence(stack[-1]))
-
-    next_val = 0
-    while len(stack) != 0:
-        next_seq = stack.pop()
-        next_val = next_seq[index] + next_val * sign
-
-    return next_val
+def next_val(sequence):
+    return (
+        0
+        if len(sequence) == 0
+        else sequence[-1]
+        + next_val([sequence[i] - sequence[i - 1] for i in range(1, len(sequence))])
+    )
 
 
 sequences = [[int(x) for x in re.findall(r"-*\d+", line)] for line in puzzle_input]
 
 
 def part_1():
-    return sum(next_value(sequence, -1, 1) for sequence in sequences)
+    return sum(map(next_val, sequences))
 
 
 def part_2():
-    return sum(next_value(sequence, 0, -1) for sequence in sequences)
+    return sum(map(next_val, map(lambda x: list(reversed(x)), sequences)))
 
 
 print(part_1())
